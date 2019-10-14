db9c712ee4cdcdd2c28e45c5b7491fe6f366117d
AbdulRahman-Diab <32856671+AbdulRahman-Diab@users.noreply.github.com>
2019-10-14 03:53:47 +0200

    Add files via upload

---
+++ AR/about_us.php
@@ -0,0 +1,164 @@
+<?php
+include "includes/header.php";
+?>
+
+<!--  main header in all Page  START  -->  
+    <section dov class=" main_page_head about_bg">
+        <div class="contact_bg main_pag_bg  ">
+            <div class="titleText">
+                <h3> من نحن </h3>
+                <ol class="list-inline text-center text-black ">
+                    <li><a href="index.php"> الرئيسية</a></li>
+                    <li class="active "> من نحن  </li>
+                </ol>
+            </div>
+        </div>
+    </section>
+<!--  main header in all Page  END  -->
+<!--  About Page  START  -->
+    <section class="about_btl">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="about_img">
+                        <img src="img/about00.jpg" class="img-responsive" alt="">
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="about_p">
+                        <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. ولذلك يتم استخدام طريقة لوريم إيبسوم لأنها تعطي توزيعاَ طبيعياَ -إلى حد ما- للأحرف عوضاً عن استخدام "هنا يوجد محتوى نصي، هنا يوجد محتوى نصي" فتجعلها تبدو (أي الأحرف) وكأنها نص مقروء. العديد من برامح النشر المكتبي وبرامح تحرير صفحات الويب تستخدم لوريم إيبسوم بشكل إفتراضي كنموذج عن النص، وإذا قمت بإدخال "lorem ipsum" في أي محرك بحث ستظهر العديد من المواقع الحديثة العهد في نتائج البحث. على مدى السنين ظهرت نسخ جديدة ومختلفة من نص لوريم إيبسوم، أحياناً عن طريق الصدفة، وأحياناً عن عمد كإدخال بعض العبارات الفكاهية إليها.هنالك العديد من الأنواع المتوفرة لنصوص لوريم إيبسوم، ولكن الغالبية تم تعديلها بشكل ما عبر إدخال بعض النوادر أو الكلمات العشوائية إلى النص. إن كنت تريد أن تستخدم نص لوريم إيبسوم ما، عليك أن تتحقق أولاً أن ليس هناك أي كلمات أو عبارات محرجة أو غير لائقة مخبأة في هذا النص. 
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+<!--  About Page  END  -->
+<!--  History Page  START  -->
+    <section class="history">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="h_block_001 ">
+                        <div class="block_head">
+                            <div class="history_title">
+                                <h3> عنوان لما حدث فى هذا الشهر </h3>
+                            </div>
+                            <div class="history_date">
+                                <h3 class="f_color_red"> مارس 2010  </h3>
+                            </div>
+                        </div>
+                        <div class="history_p">
+                            <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. ولذلك يتم استخدام طريقة لوريم إيبسوم لأنها تعطي توزيعاَ طبيعياَ -إلى حد ما- للأحرف عوضاً عن استخدام "هنا يوجد محتوى نصي، هنا يوجد محتوى نصي" فتجعلها تبدو (أي الأحرف) وكأنها نص مقروء.                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="h_block_001 ">
+                        <div class="block_head">
+                            <div class="history_title">
+                                <h3> عنوان لما حدث فى هذا الشهر </h3>
+                            </div>
+                            <div class="history_date">
+                                <h3 class="f_color_yellow"> مارس 2010   </h3>
+                            </div>
+                        </div>
+                        <div class="history_p">
+                            <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. ولذلك يتم استخدام طريقة لوريم إيبسوم لأنها تعطي توزيعاَ طبيعياَ -إلى حد ما- للأحرف عوضاً عن استخدام "هنا يوجد محتوى نصي، هنا يوجد محتوى نصي" فتجعلها تبدو (أي الأحرف) وكأنها نص مقروء.                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="h_block_001 ">
+                        <div class="block_head">
+                            <div class="history_title">
+                                <h3> عنوان لما حدث فى هذا الشهر </h3>
+                            </div>
+                            <div class="history_date">
+                                <h3 class="f_color_blue">  مارس 2010   </h3>
+                            </div>
+                        </div>
+                        <div class="history_p">
+                            <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. ولذلك يتم استخدام طريقة لوريم إيبسوم لأنها تعطي توزيعاَ طبيعياَ -إلى حد ما- للأحرف عوضاً عن استخدام "هنا يوجد محتوى نصي، هنا يوجد محتوى نصي" فتجعلها تبدو (أي الأحرف) وكأنها نص مقروء.                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="h_block_001">
+                        <div class="block_head">
+                            <div class="history_title">
+                                <h3> عنوان لما حدث فى هذا الشهر </h3>
+                            </div>
+                            <div class="history_date">
+                                <h3 class="f_color_green"> مارس 2010   </h3>
+                            </div>
+                        </div>
+                        <div class="history_p">
+                            <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. ولذلك يتم استخدام طريقة لوريم إيبسوم لأنها تعطي توزيعاَ طبيعياَ -إلى حد ما- للأحرف عوضاً عن استخدام "هنا يوجد محتوى نصي، هنا يوجد محتوى نصي" فتجعلها تبدو (أي الأحرف) وكأنها نص مقروء.                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<!--  History Page  END  -->
+<!--  Contact us  START  -->
+    <section class="contact_info">
+        <div class="container">
+            <div class="row main_contact_00">
+                <div class="col-md-10 col-md-offset-1 ">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="contact-info-item addres_border text-center">
+                                <div class="contact_details_icon">
+                                    <img src="img/map-m.png" class="img-responsive" alt="">
+                                </div>
+                                <div class="contact_details_p text-center">
+                                    <h3> العنوان </h3>
+                                    <p> مدينة نصر - القاهرة - مصر. </p>
+                                    <p> مدينة نصر - القاهرة - مصر. </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="contact-info-item phone_border text-center">
+                                <div class="contact_details_icon">
+                                    <img src="img/phone.png" class="img-responsive" alt="">
+                                </div>
+                                <div class="contact_details_p text-center">
+                                    <h3> ارقام التليفونات </h3>
+                                    <p> +20 101 23 45 678 </p>
+                                    <p> +20 101 23 45 678 </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="contact-info-item email_border text-center">
+                                <div class="contact_details_icon">
+                                    <img src="img/email.png" class="img-responsive" alt="">
+                                </div>
+                                <div class="contact_details_p text-center">
+                                    <h3> الايميل </h3>
+                                    <p> info@btl.com </p>
+                                    <p> hr@btl.com </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<!--  Contact us   END  -->
+
+<?php
+include "includes/footer.php";
+?>
---
+++ AR/blog.php
@@ -0,0 +1,223 @@
+<?php
+include "includes/header.php";
+?>
+<!--  main header in all Page  START  -->  
+    <section dov class=" main_page_head">
+        <div class="contact_bg main_pag_bg">
+            <div class="titleText">
+                <h3> المدونة </h3>
+                <ol class="list-inline text-center text-black ">
+                    <li><a href="index.php"> الرئيسية</a></li>
+                    <li class="active "> المدونة </li>
+                </ol>
+            </div>
+        </div>
+    </section>
+<!--  main header in all Page  END  -->
+
+<!--  Blog Page  START  -->  
+    <section class="blog_page">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="course_block_00 blog_block_00  c_b_hover border-black ">
+                         <a href="#"><div class="c_b_img_00 ">
+                            <img src="img/img-re9.jpg" class="img-responsive img-002">
+                             </div></a>
+                        <div class="c_b_info_00">
+                            <div class="c_b_title">
+                                <a href="#" class="course-name f_color_red"> هنا يوضع نص تجريبى لعنوان المقال او الخبر.  </a>
+                                <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. 
+                                </p>
+                                <ul class="c_b_ul_info list-inline">
+                                  <li><i class="fa fa-thumbs-o-up "></i> 13</li>
+                                  <li><i class="fa fa-comments-o "></i> 43</li>
+                                  <li class="lang-flag_2"><a href="#"><img src="img/united-kingdom.png" class="img-responsive" alt=""></a></li>
+                                </ul>
+                                <a href="#" class="b_link_2"> اقرأ الخبر كاملاً </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="course_block_00 blog_block_00  c_b_hover border-black ">
+                         <a href="#"><div class="c_b_img_00 ">
+                            <img src="img/img-re9.jpg" class="img-responsive img-002">
+                             </div></a>
+                        <div class="c_b_info_00">
+                            <div class="c_b_title">
+                                <a href="#" class="course-name f_color_red"> هنا يوضع نص تجريبى لعنوان المقال او الخبر.  </a>
+                                <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. 
+                                </p>
+                                <ul class="c_b_ul_info list-inline">
+                                  <li><i class="fa fa-thumbs-o-up "></i> 13</li>
+                                  <li><i class="fa fa-comments-o "></i> 43</li>
+                                  <li class="lang-flag_2"><a href="#"><img src="img/united-kingdom.png" class="img-responsive" alt=""></a></li>
+                                </ul>
+                                <a href="#" class="b_link_2"> اقرأ الخبر كاملاً </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="course_block_00 blog_block_00  c_b_hover border-black ">
+                         <a href="#"><div class="c_b_img_00 ">
+                            <img src="img/img-re9.jpg" class="img-responsive img-002">
+                             </div></a>
+                        <div class="c_b_info_00">
+                            <div class="c_b_title">
+                                <a href="#" class="course-name f_color_red"> هنا يوضع نص تجريبى لعنوان المقال او الخبر.  </a>
+                                <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. 
+                                </p>
+                                <ul class="c_b_ul_info list-inline">
+                                  <li><i class="fa fa-thumbs-o-up "></i> 13</li>
+                                  <li><i class="fa fa-comments-o "></i> 43</li>
+                                  <li class="lang-flag_2"><a href="#"><img src="img/united-kingdom.png" class="img-responsive" alt=""></a></li>
+                                </ul>
+                                <a href="#" class="b_link_2"> اقرأ الخبر كاملاً </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="course_block_00 blog_block_00  c_b_hover border-black ">
+                         <a href="#"><div class="c_b_img_00 ">
+                            <img src="img/img-re9.jpg" class="img-responsive img-002">
+                             </div></a>
+                        <div class="c_b_info_00">
+                            <div class="c_b_title">
+                                <a href="#" class="course-name f_color_red"> هنا يوضع نص تجريبى لعنوان المقال او الخبر.  </a>
+                                <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. 
+                                </p>
+                                <ul class="c_b_ul_info list-inline">
+                                  <li><i class="fa fa-thumbs-o-up "></i> 13</li>
+                                  <li><i class="fa fa-comments-o "></i> 43</li>
+                                  <li class="lang-flag_2"><a href="#"><img src="img/united-kingdom.png" class="img-responsive" alt=""></a></li>
+                                </ul>
+                                <a href="#" class="b_link_2"> اقرأ الخبر كاملاً </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="course_block_00 blog_block_00  c_b_hover border-black ">
+                         <a href="#"><div class="c_b_img_00 ">
+                            <img src="img/img-re9.jpg" class="img-responsive img-002">
+                             </div></a>
+                        <div class="c_b_info_00">
+                            <div class="c_b_title">
+                                <a href="#" class="course-name f_color_red"> هنا يوضع نص تجريبى لعنوان المقال او الخبر.  </a>
+                                <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. 
+                                </p>
+                                <ul class="c_b_ul_info list-inline">
+                                  <li><i class="fa fa-thumbs-o-up "></i> 13</li>
+                                  <li><i class="fa fa-comments-o "></i> 43</li>
+                                  <li class="lang-flag_2"><a href="#"><img src="img/united-kingdom.png" class="img-responsive" alt=""></a></li>
+                                </ul>
+                                <a href="#" class="b_link_2"> اقرأ الخبر كاملاً </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="course_block_00 blog_block_00  c_b_hover border-black ">
+                         <a href="#"><div class="c_b_img_00 ">
+                            <img src="img/img-re9.jpg" class="img-responsive img-002">
+                             </div></a>
+                        <div class="c_b_info_00">
+                            <div class="c_b_title">
+                                <a href="#" class="course-name f_color_red"> هنا يوضع نص تجريبى لعنوان المقال او الخبر.  </a>
+                                <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. 
+                                </p>
+                                <ul class="c_b_ul_info list-inline">
+                                  <li><i class="fa fa-thumbs-o-up "></i> 13</li>
+                                  <li><i class="fa fa-comments-o "></i> 43</li>
+                                  <li class="lang-flag_2"><a href="#"><img src="img/united-kingdom.png" class="img-responsive" alt=""></a></li>
+                                </ul>
+                                <a href="#" class="b_link_2"> اقرأ الخبر كاملاً </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="course_block_00 blog_block_00  c_b_hover border-black ">
+                         <a href="#"><div class="c_b_img_00 ">
+                            <img src="img/img-re9.jpg" class="img-responsive img-002">
+                             </div></a>
+                        <div class="c_b_info_00">
+                            <div class="c_b_title">
+                                <a href="#" class="course-name f_color_red"> هنا يوضع نص تجريبى لعنوان المقال او الخبر.  </a>
+                                <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. 
+                                </p>
+                                <ul class="c_b_ul_info list-inline">
+                                  <li><i class="fa fa-thumbs-o-up "></i> 13</li>
+                                  <li><i class="fa fa-comments-o "></i> 43</li>
+                                  <li class="lang-flag_2"><a href="#"><img src="img/united-kingdom.png" class="img-responsive" alt=""></a></li>
+                                </ul>
+                                <a href="#" class="b_link_2"> اقرأ الخبر كاملاً </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="course_block_00 blog_block_00  c_b_hover border-black ">
+                         <a href="#"><div class="c_b_img_00 ">
+                            <img src="img/img-re9.jpg" class="img-responsive img-002">
+                             </div></a>
+                        <div class="c_b_info_00">
+                            <div class="c_b_title">
+                                <a href="#" class="course-name f_color_red"> هنا يوضع نص تجريبى لعنوان المقال او الخبر.  </a>
+                                <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. 
+                                </p>
+                                <ul class="c_b_ul_info list-inline">
+                                  <li><i class="fa fa-thumbs-o-up "></i> 13</li>
+                                  <li><i class="fa fa-comments-o "></i> 43</li>
+                                  <li class="lang-flag_2"><a href="#"><img src="img/united-kingdom.png" class="img-responsive" alt=""></a></li>
+                                </ul>
+                                <a href="#" class="b_link_2"> اقرأ الخبر كاملاً </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="course_block_00 blog_block_00  c_b_hover border-black ">
+                         <a href="#"><div class="c_b_img_00 ">
+                            <img src="img/img-re9.jpg" class="img-responsive img-002">
+                             </div></a>
+                        <div class="c_b_info_00">
+                            <div class="c_b_title">
+                                <a href="#" class="course-name f_color_red"> هنا يوضع نص تجريبى لعنوان المقال او الخبر.  </a>
+                                <p>
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها. 
+                                </p>
+                                <ul class="c_b_ul_info list-inline">
+                                  <li><i class="fa fa-thumbs-o-up "></i> 13</li>
+                                  <li><i class="fa fa-comments-o "></i> 43</li>
+                                  <li class="lang-flag_2"><a href="#"><img src="img/united-kingdom.png" class="img-responsive" alt=""></a></li>
+                                </ul>
+                                <a href="#" class="b_link_2"> اقرأ الخبر كاملاً </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <nav aria-label="...">
+              <ul class="pager">
+                <li><a href="#">Previous</a></li>
+                <li><a href="#">Next</a></li>
+              </ul>
+            </nav>
+        </div>
+    </section>
+<!--  Courses Page  END  -->  
+<?php
+include "includes/footer.php";
+?>
